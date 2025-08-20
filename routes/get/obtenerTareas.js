bda430f5708abe721d6192e4225714b9a8ed750e
rockemma <jossuerm55@gmail.com>
2025-08-20 04:55:45 -0600

    formato de fecha actualizada.

--- routes/get/obtenerTareas.js
+++ routes/get/obtenerTareas.js
@@ -2,15 +2,25 @@ const db = require('../../config/database');
 const express = require('express');
 const router = express.Router();
 
-router.get('/tareas',async(req,res)=>{
-
+router.get('/tareas', async (req, res) => {
     try {
+        let [todasLasTareas] = await db.query(`
+      SELECT 
+        id,
+        nombre,
+        descripcion,
+        estado_tarea,
+        DATE_FORMAT(fecha_asignada, '%Y-%m-%d') AS fecha_asignada,
+        DATE_FORMAT(fecha_entrega, '%Y-%m-%d') AS fecha_entrega,
+        estado_artivado,
+        usuario_id
+      FROM tareas
+    `);
 
-        let [todasLasTareas] = await db.query('SELECT * FROM tareas');
         res.json(todasLasTareas);
-        
     } catch (error) {
         console.error("error: ", error);
+        res.status(500).json({ error: "Error al obtener tareas" });
     }
 });
 
